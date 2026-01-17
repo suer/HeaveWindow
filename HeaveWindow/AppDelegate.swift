7cd646b04c829c3df3585ec7e209ffce6164110c
Ryo SUETSUGU <suetsugu.r@gmail.com>
2026-01-17 10:07:37 +0900

    polling to approve accessibility check

--- HeaveWindow/AppDelegate.swift
+++ HeaveWindow/AppDelegate.swift
@@ -4,11 +4,12 @@ import SwiftUI
 class AppDelegate: NSObject, NSApplicationDelegate {
     var statusItem: NSStatusItem?
     var windowOperation: WindowOperation?
-    
+    var accessibilityCheckTimer: Timer?
+
     func applicationDidFinishLaunching(_ notification: Notification) {
-        
+
         statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
-        
+
         if let button = statusItem?.button {
             button.image = NSImage(named: "MenuIcon")
             button.action = #selector(statusBarButtonClicked)
@@ -16,18 +17,40 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         let menu = NSMenu()
         menu.addItem(NSMenuItem(title: "Quit", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
         statusItem?.menu = menu
-        checkAccessibilityPermission()
-        windowOperation = WindowOperation()
+
+        setupWithAccessibilityCheck()
     }
-    
+
     @objc func statusBarButtonClicked() {
         statusItem?.menu?.popUp(positioning: nil, at: NSEvent.mouseLocation, in: nil)
     }
-    
-    func checkAccessibilityPermission() {
-        if !AXIsProcessTrusted() {
+
+    func setupWithAccessibilityCheck() {
+        if AXIsProcessTrusted() {
+            enableWindowOperation()
+        } else {
             let options = [kAXTrustedCheckOptionPrompt.takeUnretainedValue() as String: true]
             AXIsProcessTrustedWithOptions(options as CFDictionary)
+            startAccessibilityPolling()
         }
+    }
+
+    func startAccessibilityPolling() {
+        accessibilityCheckTimer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { [weak self] _ in
+            if AXIsProcessTrusted() {
+                self?.enableWindowOperation()
+                self?.stopAccessibilityPolling()
+            }
+        }
+    }
+
+    func stopAccessibilityPolling() {
+        accessibilityCheckTimer?.invalidate()
+        accessibilityCheckTimer = nil
+    }
+
+    func enableWindowOperation() {
+        guard windowOperation == nil else { return }
+        windowOperation = WindowOperation()
     }
 }
